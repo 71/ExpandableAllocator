ee8ee87543e02f71565b44bcbe8b96c7e48e0208
Grégoire Geis <git@gregoirege.is>
2018-02-14 23:50:14 +0100

    Added seek test to StreamTests.fs.

--- ExpandableAllocator.Tests/StreamTests.fs
+++ ExpandableAllocator.Tests/StreamTests.fs
@@ -6,6 +6,7 @@ open FsUnitTyped
 open NUnit.Framework
 
 open System.IO
+open System.Text
 
 
 let newStream(protection, size: int64) = new ExpandableStream(Allocator.Create(protection, size), true)
@@ -44,3 +45,17 @@ let ``should write and read correctly``(input: string) =
         use reader = new StreamReader(stream)
 
         reader.ReadToEnd() |> shouldEqual input
+
+[<Test>]
+let ``should handle movements correctly``() =
+    use stream = newStream(Protection.ReadWrite, 0x10_000L)
+    use writer = new BinaryWriter(stream, Encoding.UTF8, true)
+    use reader = new BinaryReader(stream, Encoding.UTF8, true)
+
+    writer.Write(1)
+    writer.Write(2)
+    
+    writer.Seek(0, SeekOrigin.Begin) |> ignore
+
+    reader.ReadInt32() |> shouldEqual 1
+    reader.ReadInt32() |> shouldEqual 2
